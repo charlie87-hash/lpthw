667daebdbc9c55e614c70786d2cfeabdcaaa50bc
charles ongom <charlieongom03@gmail.com>
2022-08-27 20:32:49 -0700

    what-was-that

---
+++ ex10.py
@@ -0,0 +1,22 @@
+
+#this is my own thing
+if __name__ == "__main__":
+    print("This was executed directly")
+else:
+    print("This was imported as a module")
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishies
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
