1503f76b74f0449635b45f1b28cf711f3e18bd1f
charles ongom <charlieongom03@gmail.com>
2022-08-27 20:05:42 -0700

    printing-printing

---
+++ ex8.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    print("This was executed directly")
+else:
+    print("This was imported as a module")
+
+formatter = "{} {} {} {}"
+
+print(formatter.format(1,2,3,4))
+print(formatter.format("one", "two", "three", "four"))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+    "Try your",
+    "Own text here",
+    "Maybe a poem",
+    "Or a song about fear"))
