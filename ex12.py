c896bf5634bd1e3466b5c600acef3b2272336519
charles ongom <charlieongom03@gmail.com>
2022-08-27 20:49:19 -0700

    prompting-people

---
+++ ex12.py
@@ -0,0 +1,13 @@
+#this is my own thing
+if __name__ == "__main__":
+    print("This was executed directly")
+else:
+    print("This was imported as a module")
+
+#Prompting People
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
