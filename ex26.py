31a8c004814f51e6610c90b05fd46c2959650a80
charles ongom <charlieongom03@gmail.com>
2022-09-29 16:21:54 -0700

    ex26_Take_a_test

--- ex26.py
+++ ex26.py
@@ -7,9 +7,10 @@ from sys import argv
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
+height = input()
 print("How much do you weigh?", end=' ')
 weight = input()
-height  = input()
+
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 script, filename = argv
